e4ebb44488473e4613330ecbba126ea1f9fb4f96
rodolfocerutti <42013617+rodolfocerutti@users.noreply.github.com>
2022-07-28 21:23:14 -0300

    Terceira modificação tesla-clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Header from "./components/Header"
 
+import Accesories from "./assets/Desktop-Accessories.jpg"
+import ModelS from "./assets/Desktop-ModelS.jpeg"
+import Model3 from "./assets/Desktop-Model3.jpeg"
+import ModelX from "./assets/Desktop-ModelX.jpeg"
+import ModelY from "./assets/Desktop-ModelY.jpeg"
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg"
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg"
+
+
 function App() {
   return (
     <div className="App">
-      <Header /> 
+      <Header />
+      <div className='app__itemsContainer'>
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+const Item = () => {
+    return (
+        <div className="item" style={{
+
+        }}>
+            <div className="item__container">
+                <div className="item__text">
+                    <p>Title</p>
+                    <div className="item__textDesc">
+                        <p>Desc</p>
+                    </div>
+                </div>
+                <div className="item__lowerThird">
+                    <div className="item__buttons">
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+return default Item
